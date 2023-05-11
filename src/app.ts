e8f423d75d548b32e0bd92c2f4cc3c167866f729
Andrii Kicha <akicha@star.global>
2023-05-11 12:19:48 -0600

    create game animation loop

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import { Canvas } from './canvas'
-import { Map } from './map'
-import { Player } from './player'
+import { Canvas } from "./canvas";
+import { Map } from "./map";
+import { Player } from "./player";
+
+function animate() {
+  Map.draw();
+  Player.draw();
+  window.requestAnimationFrame(animate);
+}
 
 export async function main() {
   Canvas.init();
   await Map.init();
   Player.init();
+  animate();
 }
--- src/player/index.ts
+++ src/player/index.ts
@@ -3,24 +3,28 @@ import { PLAYER_ENUMS } from "./enums";
 import { Canvas } from "../canvas";
 
 export class Player {
+  static playerImage: HTMLImageElement;
   static init() {
     const playerImage = new Image();
     playerImage.src = playerMovementSprite;
-    playerImage.onload = function handleImageLoaded() {
-      const canvas = Canvas.getCanvas();
-      const SINGLE_PRESET_WIDTH =
-        playerImage.width / PLAYER_ENUMS.NUMBER_OF_MOVEMENT_SPRITE_PRESETS;
-      Canvas.getCtx().drawImage(
-        playerImage,
-        0,
-        0,
-        SINGLE_PRESET_WIDTH,
-        playerImage.height,
-        canvas.width / 2 - SINGLE_PRESET_WIDTH / 2,
-        canvas.height / 2 - SINGLE_PRESET_WIDTH / 2,
-        SINGLE_PRESET_WIDTH,
-        playerImage.height
-      );
-    };
+    Player.playerImage = playerImage;
+    playerImage.onload = Player.draw;
+  }
+
+  static draw() {
+    const canvas = Canvas.getCanvas();
+    const SINGLE_PRESET_WIDTH =
+      Player.playerImage.width / PLAYER_ENUMS.NUMBER_OF_MOVEMENT_SPRITE_PRESETS;
+    Canvas.getCtx().drawImage(
+      Player.playerImage,
+      0,
+      0,
+      SINGLE_PRESET_WIDTH,
+      Player.playerImage.height,
+      canvas.width / 2 - SINGLE_PRESET_WIDTH / 2,
+      canvas.height / 2 - SINGLE_PRESET_WIDTH / 2,
+      SINGLE_PRESET_WIDTH,
+      Player.playerImage.height
+    );
   }
 }
