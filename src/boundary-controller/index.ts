def4556f30c1f285da2a763227dee6e3ac85ad93
Andrii Kicha <akicha@star.global>
2023-05-17 19:56:55 -0600

    create new Boundary object for each of the collision appearance

--- src/boundary-controller/index.ts
+++ src/boundary-controller/index.ts
@@ -1,15 +1,17 @@
 import { Boundary } from "../boundary";
 
 export class BoundaryController {
-  static boundaries: { x: number; y: number }[];
+  static boundaries: Boundary[];
 
   static init(boundaries: { x: number; y: number }[]) {
-    BoundaryController.boundaries = boundaries;
+    BoundaryController.boundaries = boundaries.map(
+      (boundary) => new Boundary(boundary)
+    );
   }
 
   static draw() {
     BoundaryController.boundaries.forEach((boundary) => {
-      Boundary.draw(boundary);
+      boundary.draw();
     });
   }
 }
