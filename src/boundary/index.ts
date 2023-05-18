f75764a8a840d73f0ff1e25238cce5daadadca22
Andrii Kicha <akicha@star.global>
2023-05-18 08:13:13 -0600

    add collisions detection logic

--- src/boundary/index.ts
+++ src/boundary/index.ts
@@ -4,6 +4,7 @@ import { MAP_ENUMS } from "../map/enums";
 export class Boundary {
   x: number;
   y: number;
+  static size = MAP_ENUMS.TILE_SIZE * (MAP_ENUMS.IMAGE_ZOOM_PERCENTS / 100);
   constructor({ x, y }: { x: number; y: number }) {
     this.x = x;
     this.y = y;
@@ -15,8 +16,8 @@ export class Boundary {
     ctx.fillRect(
       this.x + MAP_ENUMS.INITIAL_MAP_POSITION_X,
       this.y + MAP_ENUMS.INITIAL_MAP_POSITION_Y,
-      MAP_ENUMS.TILE_SIZE * (MAP_ENUMS.IMAGE_ZOOM_PERCENTS / 100),
-      MAP_ENUMS.TILE_SIZE * (MAP_ENUMS.IMAGE_ZOOM_PERCENTS / 100)
+      Boundary.size,
+      Boundary.size
     );
   }
 
--- src/player-boundary-collision-controller/index.ts
+++ src/player-boundary-collision-controller/index.ts
@@ -1,5 +1,41 @@
 import { Player } from "../player";
+import { BoundaryController } from "../boundary-controller";
+import { Boundary } from "../boundary";
 
 export class PlayerBoundaryCollisionController {
-  static isCollisionDetected() {}
+  static rectangularCollision(
+    box1: { x: number; y: number; width: number; height: number },
+    box2: { x: number; y: number; width: number; height: number }
+  ) {
+    return (
+      box1.x + box1.width >= box2.x &&
+      box1.x <= box1.x + box1.width &&
+      box1.y + box1.height <= box2.y &&
+      box1.y >= box1.y + box1.height
+    );
+  }
+  static isCollisionDetected() {
+    for (let boundary of BoundaryController.getBoundaries()) {
+      if (
+        PlayerBoundaryCollisionController.rectangularCollision(
+          {
+            x: Player.x,
+            width: Player.playerImage.width,
+            y: Player.y,
+            height: Player.playerImage.height,
+          },
+          {
+            x: boundary.x,
+            width: Boundary.size,
+            y: boundary.y,
+            height: Boundary.size,
+          }
+        )
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
