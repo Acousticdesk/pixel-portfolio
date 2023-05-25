6bad5acc23cc0a01ef9da1ae91fd3a83159833ce
Andrii Kicha <akicha@star.global>
2023-05-25 11:00:00 -0600

    fix bug with the interaction area with NPCs

--- src/player-dialog-area-collision-controller/index.ts
+++ src/player-dialog-area-collision-controller/index.ts
@@ -1,39 +1,42 @@
 import { Player } from "../player";
 import { Boundary } from "../boundary";
-import { MAP_ENUMS } from "../map/enums";
 import { DialogAreaController } from "../dialog-area-controller";
-import { PLAYER_ENUMS } from "../player/enums";
 import { Collision } from "../collision";
 import { Map } from "../map";
 
 export class PlayerDialogAreaCollisionController {
+  // the app needs this gap to show the dialog window
+  // when a user is close to the area of interaction with NPC
+  static createGapAreaAroundPlayer() {
+    const gapSize = 32;
+    const originalPlayerPositionX = Player.x - Map.initialImageOffsetX;
+    const originalPlayerPositionY = Player.y - Map.initialImageOffsetY;
+    const playerImage = Player.sprite.getImage();
+
+    return {
+      x:
+        originalPlayerPositionX -
+        (Player.SINGLE_PRESET_WIDTH + gapSize) / 2 +
+        Player.SINGLE_PRESET_WIDTH / 2,
+      y:
+        originalPlayerPositionY -
+        (playerImage.height + gapSize) / 2 +
+        playerImage.height / 2,
+      width: Player.SINGLE_PRESET_WIDTH + gapSize,
+      height: playerImage.height + gapSize,
+    };
+  }
   static findCollisionTile() {
-    // todo akicha: fix issue with player with offset detection at the bottom of the table
-    // when a or d key is pressed
-    const offsetX =
-      Player.movementDirection === PLAYER_ENUMS.MOVEMENT_DIRECTION_RIGHT
-        ? 5
-        : -5;
-    const offsetY =
-      Player.movementDirection === PLAYER_ENUMS.MOVEMENT_DIRECTION_UP
-        ? -10
-        : 10;
     for (let dialogArea of DialogAreaController.getDialogAreas()) {
+      const playerWithGapArea =
+        PlayerDialogAreaCollisionController.createGapAreaAroundPlayer();
       if (
-        Collision.rectangularCollision(
-          {
-            x: Player.x + offsetX - Map.initialImageOffsetX,
-            y: Player.y + offsetY - Map.initialImageOffsetY,
-            width: Player.SINGLE_PRESET_WIDTH,
-            height: Player.sprite.getImage().height,
-          },
-          {
-            x: dialogArea.x,
-            width: Boundary.size,
-            y: dialogArea.y,
-            height: Boundary.size,
-          }
-        )
+        Collision.rectangularCollision(playerWithGapArea, {
+          x: dialogArea.x,
+          width: Boundary.size,
+          y: dialogArea.y,
+          height: Boundary.size,
+        })
       ) {
         return dialogArea;
       }
