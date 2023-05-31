70df33f4ce18d7e220918dcca4b300aa62c4f4ff
Andrii Kicha <akicha@star.global>
2023-05-31 13:18:40 -0600

    add project 2 template

---
+++ src/projects/templates/project-2.ts
@@ -0,0 +1,24 @@
+export const project2Template = `
+<h3 class="project__heading">Position</h3>
+<p>Senior Software Engineer</p>
+<p>Kyiv, Ukraine - 2021 - 2023</p>
+<h3 class="project__heading">Project</h3>
+<p>Star</p>
+<h3 class="project__heading">Project description</h3>
+<p>An application in HealthTech domain that allows the eyewear selection process by combining 3D modeling, augmented reality, and e-commerce. Customers can view realistic avatars of themselves, try on glasses frames virtually, and seamlessly place orders. The application offers an extensive catalog, personalized customization options, and additional features such as favorites lists.</p>
+<h3 class="project__heading">Technologies used</h3>
+<p>Angular, RxJS, NgRX, Ionic, Capacitor, Azure Pipelines, Jest, HTML 5 Canvas, App Center, Testflight</p>
+<h3 class="project__heading">Responsibilities</h3>
+<ul class="project__list">
+  <li>Collaborate with project managers, stakeholders, and other team leaders to define project scope, objectives, and deliverables</li>
+  <li>Provide leadership and guidance to the front-end development team</li>
+  <li>Collaborate with cross-functional teams to ensure smooth coordination and integration of front-end components</li>
+  <li>Design, develop, test, and deploy high-quality software solutions, ensuring adherence to coding standards, best practices, and project requirements</li>
+  <li>Conduct code reviews to ensure code quality, maintainability, and adherence to coding standards. Implement and enforce best practices for testing and quality assurance processes</li>
+  <li>Work closely with project managers and cross-functional teams to plan and prioritize software development tasks, estimate project timelines, and provide technical insights for decision-making</li>
+  <li>Create and maintain technical documentation, including system architecture diagrams, API documentation, user guides, and knowledge base articles to facilitate effective communication and knowledge sha</li>
+  <li>Identify and optimize performance bottlenecks in the software</li>
+  <li>Investigate and resolve complex technical issues reported by users or discovered during testing or production</li>
+  <li>Continuously upgrade technical skills</li>
+</ul>
+`;
