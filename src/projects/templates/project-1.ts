077915ab077a5b15a07b035bbdc3d3aee56b77fa
Andrii Kicha <akicha@star.global>
2023-05-31 12:04:28 -0600

    added initial markup for the project description

---
+++ src/projects/templates/project-1.ts
@@ -0,0 +1,24 @@
+export const project1Template = `
+  <h4>Position</h4>
+  <p>FE Team Leader</p>
+  <p>Kyiv, Ukraine - 2022 - 2022</p>
+  <h4>Project</h4>
+  <p>Star</p>
+  <h4>Project description</h4>
+  <p>Team performance and feedback review system. This project aimed to enhance team productivity, identify areas for improvement, and foster a culture of continuous growth and development.</p>
+  <h4>Technologies used</h4>
+  <p>Typescript, React, Redux Toolkit, MUI, Jest, React Testing Library, Sentry, React Intl, Storybook</p>
+  <h4>Responsibilities</h4>
+  <ul>
+    <li>Provide leadership and guidance to the front-end development team</li>
+    <li>Address performance or behavioral issues proactively, providing necessary support and guidance</li>
+    <li>Assign tasks, set priorities, and monitor progress to ensure timely and successful project completion</li>
+    <li>Foster a positive team culture, encouraging collaboration, knowledge sharing, and innovation</li>
+    <li>Collaborate with project managers, stakeholders, and other team leaders to define project scope, objectives, and deliverables</li>
+    <li>Ensure adherence to coding standards, best practices, and quality assurance processes</li>
+    <li>Help team members troubleshoot and resolve complex technical issues</li>
+    <li>Liaise with UX/UI designers and product managers to understand design requirements and translate them into functional front-end solutions</li>
+    <li>Collaborate with cross-functional teams to ensure smooth coordination and integration of front-end components</li>
+<li>Act as a bridge between the front-end team and other stakeholders, facilitating effective communication and information sharing</li>
+</ul>
+`;
