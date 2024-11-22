0a464f766d3b3559df2e3bb224682520f9546c9c
Andrii Kicha <akicha@star.global>
2024-11-22 16:42:33 -0700

    started working on the extended map

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { main } from "./app";
 
 // todo akicha: add a room with open source projects
 // todo akicha: think about how to present experience
+// todo akicha: Wall Contours and Room Borders layers are used inconsistently
 main();
