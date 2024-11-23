5b910d72ccb18caf8defc309a1fa19e1b21de291
Andrii Kicha <akicha@star.global>
2024-11-23 08:43:20 -0700

    added another room to the new map

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { main } from "./app";
 
 // todo akicha: add a room with open source projects
 // todo akicha: think about how to present experience
-// todo akicha: Wall Contours and Room Borders layers are used inconsistently
+// todo akicha: idea to show the resume: an HR asks the player to pass the resume to another NPC
+// todo akicha: the rooms are too small
 main();
